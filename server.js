b39e9e87017dc943fe898c2c2cca742c70c8f3ca
Maria Vitória Silva <mariavitoriasilvast@gmail.com>
2022-09-29 18:13:39 -0300

    fix: database name update

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const routes = require('./src/routes');
 const app = express();
 const port = process.env.PORT || 5000;
 
-  mongoose.connect('mongodb://localhost:27017/site-vagas-de-emprego', {
+  mongoose.connect('mongodb://localhost:27017/site-de-vagas-de-emprego', {
     useUnifiedTopology: true,
     useNewUrlParser: true,
     useFindAndModify: false
